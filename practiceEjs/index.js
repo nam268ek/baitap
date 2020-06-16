45de46dcb96cfe7cff3fb55eaf4a1e3cd7f9d569
nam268ek <namnguyenexe@gmail.com>
2020-06-16 20:43:53 +0700

    test

--- practiceEjs/index.js
+++ practiceEjs/index.js
@@ -2,8 +2,9 @@ var express = require("express");
 var app = express();
 var ejsRouters = require("./routers/ejs.route");
 
-app.set("view engine", "ejs");
+
 app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.use("/", ejsRouters);
 
